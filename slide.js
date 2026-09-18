d3072217d4ef8ad4b66c3a10af7a2951b862f1a3
danit24798-wq <danit24798@gmail.com>
2026-09-18 12:27:03 -0700

    Refactor image and video slider functions

--- slide.js
+++ slide.js
@@ -1,34 +1,51 @@
+// ===============================
+// IMAGE SLIDER
+// ===============================
+let slideIndex = 1;
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-} 
-function plusSlidesVd(n) {
-  showSlidesVid(slideIndex += n);
-} 
+    showSlides(slideIndex += n);
+}
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlideB");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  } 
-  slides[slideIndex-1].style.display = "block";  
-}
+    let i;
+    let slides = document.getElementsByClassName("mySlideB");
 
-function showSlidesVid(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlideVid");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  } 
-  slides[slideIndex-1].style.display = "block";  
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
-let slideIndex = 1;
-showSlides(slideIndex);  
-showSlidesVid(slideIndex); 
+// ===============================
+// VIDEO SLIDER
+// ===============================
+let slideIndexVid = 1;
+function plusSlidesVd(n) {
+    showSlidesVid(slideIndexVid += n);
+}
+function showSlidesVid(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlideVid");
+
+    if (n > slides.length) {
+        slideIndexVid = 1;
+    }
+    if (n < 1) {
+        slideIndexVid = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndexVid - 1].style.display = "block";
+} 
+
+showSlides(slideIndex);
+showSlidesVid(slideIndexVid);
